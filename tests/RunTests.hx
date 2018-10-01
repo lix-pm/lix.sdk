c078b00aca34397c658d517cc607a92f02b04ad6
Kevin Leung <kevinresol@gmail.com>
2018-10-02 01:27:22 +0800

    Test /me endpoint

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -7,9 +7,13 @@ using tink.CoreApi;
 class RunTests {
   static function main() {
     var auth = new lix.Auth();
-    trace(auth.isSignedIn());
-    auth.getSession().handle(function(o) trace(o.sure()));
-    var remote = new lix.Remote(new SecureNodeClient(), () -> auth.getSession().next(session -> session.idToken));
+    // trace(auth.isSignedIn());
+    // auth.getSession().handle(function(o) trace(o.sure()));
+    var remote = new lix.Remote(
+      #if (environment == "local") new NodeClient() #else new SecureNodeClient() #end, 
+      () -> auth.getSession().next(session -> session.idToken)
+    );
     remote.version().handle(function(o) trace(o));
+    remote.me().get().handle(function(o) trace(o));
   }
 }
