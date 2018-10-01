2ad946f6a1c44a50a55c2f9f18e3c15fac2bdfad
Kevin Leung <kevinresol@gmail.com>
2018-10-02 01:37:02 +0800

    Try creating a new user

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -13,7 +13,24 @@ class RunTests {
       #if (environment == "local") new NodeClient() #else new SecureNodeClient() #end, 
       () -> auth.getSession().next(session -> session.idToken)
     );
-    remote.version().handle(function(o) trace(o));
-    remote.me().get().handle(function(o) trace(o));
+    
+    remote.version().handle(function(o) switch o {
+      case Success(v): trace(v.hash.substr(0, 8) + ' ' + v.buildDate);
+      case Failure(e): trace(e);
+    });
+    
+    remote.me().get()
+      .flatMap(o -> switch o {
+        case Success(user):
+          Promise.lift(user);
+        case Failure(e) if(e.code == 404):
+          remote.users().create({username: 'lix'});
+        case Failure(e):
+          Promise.lift(e);
+      })
+      .handle(function(o) switch o {
+        case Success(user): trace('Logged in as ${user.username}');
+        case Failure(e): trace(e);
+      });
   }
 }
